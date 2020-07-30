fa2f9fb2b7a36c76c7eae0ad754ac01f0cca19a7
Nguyễn Trung Nam <namnt>
2020-07-30 14:12:35 +0700

    init project

---
+++ Suggestion/src/main/java/com/example/recomendation/RecomendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.recomendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecomendationApplication.class, args);
+    }
+
+}
---
+++ Suggestion/src/main/java/com/example/recomendation/model/Suggestion.java
@@ -0,0 +1,9 @@
+package com.example.recomendation.model;
+
+import java.util.Date;
+
+public class Suggestion {
+    private String recomentId;
+    private String userName;
+    private Date datetime;
+}
